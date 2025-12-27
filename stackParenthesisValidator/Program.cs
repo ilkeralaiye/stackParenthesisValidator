1998d5ee245c48062bf7eb849c9b9ac3bd4bcb52
ilkeralaiye <ilkeralaiye@hotmail.com>
2025-12-27 16:09:15 +0300

    feat: parenthesis validation logic

--- stackParenthesisValidator/Program.cs
+++ stackParenthesisValidator/Program.cs
@@ -3,8 +3,8 @@
     internal class Stack<T> {
 
         private T[] stackArray;
-        private int capacity;
-        private int length;
+        public int capacity;
+        public int length;
 
         public Stack(int capacity) {
             this.capacity = capacity;
@@ -54,7 +54,52 @@
     
     internal class Program {
         static void Main(string[] args) {
+
+            string stringToTest = "{x+(y-[a+b])*c-[(d+e)]}/(h-(j-(k-[l-n])))";
+
             // Initial Commit
+
+            Stack<char> testStack = new Stack<char>(stringToTest.Length);
+
+            
+
+            int capacity = testStack.capacity;
+            for (int i=0;i<capacity;i++) {
+
+                char temp = stringToTest[i];
+
+                if (temp == '(' || temp == '[' || temp == '{') {
+                    testStack.Push(temp);
+                }else if (temp == ')') {
+
+                    if (testStack.Pop() != '(') throw new InvalidOperationException("This stack is not valid.");
+
+                } else if (temp == ']') {
+
+                    if (testStack.Pop() != '[') throw new InvalidOperationException("This stack is not valid.");
+
+                } else if (temp == '}') {
+
+                    if (testStack.Pop() != '{') throw new InvalidOperationException("This stack is not valid.");
+
+                }
+
+
+            }
+
+            if (testStack.length != 0) {
+                throw new InvalidOperationException("This stack is not valid.\n\n");
+            }
+
+            Console.WriteLine("String is valid");
+            
+
+
+
         }
+
+        
+
+
     }
 }
