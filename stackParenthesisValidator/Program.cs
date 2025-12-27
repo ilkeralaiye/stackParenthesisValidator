617fd0fda775a09bf4f5a06eaecea6926209f860
ilkeralaiye <ilkeralaiye@hotmail.com>
2025-12-27 15:41:00 +0300

    feat: Generic Stack class implemented.

--- stackParenthesisValidator/Program.cs
+++ stackParenthesisValidator/Program.cs
@@ -1,4 +1,57 @@
 ﻿namespace stackParenthesisValidator {
+    
+    internal class Stack<T> {
+
+        private T[] stackArray;
+        private int capacity;
+        private int length;
+
+        public Stack(int capacity) {
+            this.capacity = capacity;
+            this.stackArray = new T[this.capacity];
+            this.length = 0;
+        }
+
+        public void Push(T data) {
+
+            if (length != capacity) {
+                stackArray[length++] = data;
+            } else throw new InvalidOperationException("This stack's capacity has been reached.");
+
+        }
+
+        public T Pop() {
+
+            if (length != 0) {
+
+                T itemToReturn = stackArray[--length];
+                stackArray[length] = default(T);
+
+                return itemToReturn;
+            } else throw new InvalidOperationException("This stack is null.");
+
+
+        }
+
+        public T Peek() {
+
+            if (length != 0) {
+                return stackArray[length-1];
+            } else throw new InvalidOperationException("This stack is null.");
+
+        }
+
+
+        public bool IsEmpty() {
+            return length == 0;
+        }
+
+
+
+    }
+    
+    
+    
     internal class Program {
         static void Main(string[] args) {
             // Initial Commit
